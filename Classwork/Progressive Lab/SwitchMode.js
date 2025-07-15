ad91a4dcb40650055f3e559027d8fde4c9bff949
Izzy :3 <60242236+MRL1002@users.noreply.github.com>
2025-07-15 15:53:11 -0600

    feat: implement switch mode button and add light mode functionality

--- Classwork/Progressive Lab/SwitchMode.js
+++ Classwork/Progressive Lab/SwitchMode.js
@@ -1,3 +1,39 @@
-// starting mode will always be dark
-var currMode = "Dark";
+// starting mode will always be dark (as it should be)
+let lightmode = localStorage.getItem("lightmode");
+
+const modeswitch = document.getElementById("switch-mode");
+
+
+const enableLightmode = () => {
+    document.body.classList.add("lightmode")
+    localStorage.setItem("lightmode", "active")
+    modeswitch.textContent = "Enable Dark Mode"
+}
+
+const disableLightmode = () => {
+    document.body.classList.remove("lightmode")
+    localStorage.setItem("lightmode", null)
+}
+
+
+if(lightmode=="active"){
+    enableLightmode();
+}
+
+modeswitch.addEventListener("click", () => {
+
+    lightmode = localStorage.getItem("lightmode")
+    if(lightmode != "active") {
+        enableLightmode();
+    } 
+    else{
+        disableLightmode();
+        modeswitch.textContent = "Enable Light Mode"
+    }
+
+    
+
+});
+
+
 
