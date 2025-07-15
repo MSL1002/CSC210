1387b73c519d30511aa1a4aa4639061ecd4becd4
Izzy J. <8mrl1002@gmail.com>
2025-07-15 09:43:28 -0600

    implemented score system into RPS

--- Classwork/Notes/RPS_Script.js
+++ Classwork/Notes/RPS_Script.js
@@ -13,36 +13,47 @@ clickButton.addEventListener("click", () =>{
 
 });
 
+
 // RPS game
+
+// var = global
+var rpsScore = 0;
+
 const choices = ["rock", "paper", "scissors"]
 
 const playerChoiceDisplay = document.getElementById("player-choice")
 const computerChoiceDisplay = document.getElementById("comp-choice")
 const resultDisplay = document.getElementById("rps-result")
+const rpsScoreDisplay = document.getElementById("rps-score")
 
 
 document.querySelectorAll(".rps").forEach(button => {
-    button.addEventListener("click", () =>{
-        
-    const playerChoice = button.dataset.choice;
+    button.addEventListener("click", () => {
 
-    // gives random num between 0 and 2, math.random() gives a number between 0 and 1
-    // mult that by three and round down to 
-    const compChoice = choices[Math.floor(Math.random()*3)];
+        const playerChoice = button.dataset.choice;
 
-    playerChoiceDisplay.textContent = playerChoice;
-    computerChoiceDisplay.textContent = compChoice;
+        // gives random num between 0 and 2, math.random() gives a number between 0 and 1
+        // mult that by three and round down to
+        const compChoice = choices[Math.floor(Math.random()*3)];
 
-    resultDisplay.textContent = getResult(playerChoice, compChoice);
+        playerChoiceDisplay.textContent = playerChoice;
+        computerChoiceDisplay.textContent = compChoice;
+
+        resultDisplay.textContent = getResult(playerChoice, compChoice);
+
+        rpsScoreDisplay.textContent = rpsScore;
+
     });
 });
 
-function getResult(player,comp){
-    if(player === comp) return "TIE";
-    else if(player === "rock" && comp === "scissors" 
-        || player === "paper" && comp === "rock" 
-        || player === "scissors" && comp === "paper")
-         return "You Won!";
-    else return "Computer Won!";
+function getResult(player,comp) {
+    if(player == comp) return "TIE";
+    else if(player == "rock" && comp == "scissors" 
+        || player == "paper" && comp == "rock" 
+        || player == "scissors" && comp == "paper")
+         {rpsScore += 1
+         return "You Won!";}
+    else {rpsScore -=1
+        return "Computer Won!"};
 }
 
