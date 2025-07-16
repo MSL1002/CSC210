2dfc481defb59c4daafb72c68b2674655315a50d
Izzy J. <8mrl1002@gmail.com>
2025-07-16 09:26:05 -0600

    feat: update stylesheets and improve light/dark mode functionality

--- Classwork/Progressive Lab/SwitchMode.js
+++ Classwork/Progressive Lab/SwitchMode.js
@@ -1,39 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
 // starting mode will always be dark (as it should be)
-let lightmode = localStorage.getItem("lightmode");
 
-const modeswitch = document.getElementById("switch-mode");
+    //looks into browser's memory and gets lightmode value
+    let lightmode = localStorage.getItem("lightmode");
 
-
-const enableLightmode = () => {
-    document.body.classList.add("lightmode")
-    localStorage.setItem("lightmode", "active")
-    modeswitch.textContent = "Enable Dark Mode"
-}
-
-const disableLightmode = () => {
-    document.body.classList.remove("lightmode")
-    localStorage.setItem("lightmode", null)
-}
+    const modeswitch = document.getElementById("switch-mode");
 
 
-if(lightmode=="active"){
-    enableLightmode();
-}
-
-modeswitch.addEventListener("click", () => {
-
-    lightmode = localStorage.getItem("lightmode")
-    if(lightmode != "active") {
-        enableLightmode();
-    } 
-    else{
-        disableLightmode();
-        modeswitch.textContent = "Enable Light Mode"
+    const enableLightmode = () => {
+        document.body.classList.add("lightmode")
+        localStorage.setItem("lightmode", "active")
+        
+        modeswitch.textContent = "Enable Dark Mode"
     }
 
-    
+    const disableLightmode = () => {
+        document.body.classList.remove("lightmode")
+        localStorage.setItem("lightmode", null)
+    }
 
+    // if the lightmode value = active, enables lightmode
+    if(lightmode=="active"){
+        enableLightmode();
+    }
+
+    modeswitch.addEventListener("click", () => {
+
+        lightmode = localStorage.getItem("lightmode")
+        if(lightmode != "active") {
+            enableLightmode();
+        } 
+        else{
+            disableLightmode();
+            modeswitch.textContent = "Enable Light Mode"
+        }
+
+    });
 });
+
 
 
 
